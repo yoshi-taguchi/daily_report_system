b273f93b0c5c7adb39b927a5ba64d9c27728662a
yoshi <yoshi-taguchi@outlook.jp>
2019-05-19 02:01:32 +0900

    Add Employee Index/ Modify persistence.xml

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -11,19 +11,19 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-@Table(name="employees")
+@Table(name = "employees")
 @NamedQueries({
     @NamedQuery(
-            name="getAllEmployees",
+            name = "getAllEmployees",
             query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
             ),
     @NamedQuery(
             name = "getEmployeesCount",
-            query = "SELECT count(e) FROM Employee AS e"
+            query = "SELECT COUNT(e) FROM Employee AS e"
             ),
     @NamedQuery(
             name = "checkRegisteredCode",
-            query = "SELECT count(e) FROM Employee AS e WHERE e.code = :code"
+            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
             ),
     @NamedQuery(
             name = "checkLoginCodeAndPassword",
