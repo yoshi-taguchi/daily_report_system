fba309e10df0615e5543cf65bbf0ba3354a99b5a
yoshi <yoshi-taguchi@outlook.jp>
2019-05-23 04:25:54 +0900

    Add report create/show

--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -1,8 +1,8 @@
 package controllers.reports;
 
 import java.io.IOException;
-import java.sql.Date;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,18 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Report;
+import utils.DBUtil;
 
 /**
- * Servlet implementation class ReportsNewServlet
+ * Servlet implementation class ReportsShowServlet
  */
-@WebServlet("/reports/new")
-public class ReportsNewServlet extends HttpServlet {
+@WebServlet("/reports/show")
+public class ReportsShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReportsNewServlet() {
+    public ReportsShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,13 +32,16 @@ public class ReportsNewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("_token", request.getSession().getId());
+        EntityManager em = DBUtil.createEntityManager();
 
-        Report r = new Report();
-        r.setReport_date(new Date(System.currentTimeMillis()));
+        Report r = em.find(Report.class,Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
         request.setAttribute("report", r);
+        request.setAttribute("_token",request.getSession().getId() );
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
         rd.forward(request, response);
     }
 
